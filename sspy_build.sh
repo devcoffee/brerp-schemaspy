33e4c542c97de08bac4cca1ee3c9ee975ac28d7b
muriloht <muriloht@devcoffee.com.br>
2023-04-20 11:36:31 -0300

    COF #11089 - adicionando projeto para geracao de schemaspy

---
+++ sspy_build.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+# 
+# Script para geração automatizada do SchmaSpy à partir de um BD BrERP/iDempiere
+# 
+# Antes de executar, assegure-se de ter o java e o pacote graphviz instalados
+# e configurados em seu ambiente
+#
+# 
+
+DOTFILE="config.env"
+DIR="./public"
+
+REQUIRED_TOOLS=(
+  "java"
+  "dot"
+)
+
+SCHEMASPY_JAR="lib/schemaspy-6.2.2.jar"
+# Run: java -jar lib/schemaspy-6.1.0.jar -dbhelp
+# Even though we are connecting to Postgres version 13, the highest database type listed is pgsql11 ... 
+DATABASE_TYPE="pgsql11"
+JDBC_DRIVERS="lib/driver"
+
+for tool in ${REQUIRED_TOOLS[@]}; do
+  if ! command -v ${tool} >/dev/null; then
+    echo "${tool} is required ..."
+    exit 1
+  fi
+done
+
+if [[ ! -f ${DOTFILE} ]]; then
+  echo "File ${DOTFILE} is required ..."
+  exit 1
+fi
+
+source ${DOTFILE}
+
+# See https://schemaspy.readthedocs.io/en/latest/configuration/commandline.html
+java -jar ${SCHEMASPY_JAR} -debug -imageformat svg -degree 1 -t ${DATABASE_TYPE} -dp ${JDBC_DRIVERS} -host ${PGHOST} -port ${PGPORT} -db ${PGDATABASE} -s ${PGSCHEMA} -u ${PGUSER} -p ${PGPASSWORD} -o ${DIR}
+
+cp -pr CNAME public/
+
+echo "Completed ${0} in ${SECONDS}s"
